d9b52413fff1fc8fb3b3e818d4eaf48ff786afe0
Ivan Gamboa <chavez.ivan@correounivalle.edu.co>
2025-11-17 23:30:20 -0500

    Modificacion_BD

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   host: 'localhost',
-  port: 5432,
+  port: 5433,
   user: 'postgres',
   password: '123', // tu contraseña de PostgreSQL
   database: 'app_basura'
